cd405aef3f9939cf093a53b8bfe9905e072aa1f4
Enilson Neto01~ <enilsonneto8@gmail.com>
2022-11-29 20:06:44 -0300

    Atualizando a tabela de notas de alunos, para aumentar as possibilidades.

--- Nota_Alunos/src/App.java
+++ Nota_Alunos/src/App.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App 
+{
+    public static void main(String[] args)
+    {
         Scanner scan = new Scanner (System.in);
-        float nota = 0;
-     
-        for (int i = 1;
-                    i < 11; 
-                        i++) 
-                        
-           {float number = 0;
-            System.out.println("Nota do aluno " + i + " = ");
-
-            number = scan.nextFloat();
-            nota += number;
-        }
+        Scanner yorn = new Scanner (System.in);
+        float nota = 0f;
+        int yn = 0;
+        while (yn == 0)
+        {
+            for (int i = 1;i < 11; i++)
+                {
+                    float number = 0f;
+                    System.out.println("Nota do aluno " + i + " = ");
+                    number = scan.nextFloat();
+                    nota += number;
+                }
+            System.out.println("A média do aluno é " + (nota/10));
+            nota = 0f;
+            System.out.println("Deseja continuar inserindo notas? se sim digite '0' , se não Digite '-1'");
+            yn = yorn.nextInt();
+        } 
         scan.close();
-        System.out.println("A média do aluno é " + (nota/10));
+        yorn.close();
     }
 }
